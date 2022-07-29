88cc225cf4b25820ad2bdc1144aedc1c2ff1119a
Marcus Almeida <marcusmage@gmail.com>
2022-07-29 12:29:51 -0300

    Event sourcing

---
+++ src/main/java/com/bank/BankCrqsEsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCrqsEsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCrqsEsMicroserviceApplication.class, args);
+	}
+
+}
